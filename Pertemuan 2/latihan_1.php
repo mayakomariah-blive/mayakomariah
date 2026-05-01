687846c444bc2bdbee4b350a7f91195783f47c5c
mayakomariah-blive <qomariahmaya6@gmail.com>
2026-05-01 20:11:56 +0700

    Add files via upload

---
+++ Pertemuan 2/latihan_1.php
@@ -0,0 +1,13 @@
+<?php
+$a = 1;
+$b = 2;
+$c = $a.$b;   // hasil: "12"
+$d = $c + 1;  // hasil: 13
+echo $d;
+
+$e = "Number";
+$f = $e.$d;   // hasil: "Number13"
+echo $f;
+?>
+
+//BY NUR ILMI - 221011400755
